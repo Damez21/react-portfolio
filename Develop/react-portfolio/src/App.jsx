525df42470f9bb77a903ccd591e912e6ff4b0cd8
Damiem Mclaurin <damiemmclaurin@Damiems-MacBook-Pro.local>
2023-08-14 23:03:13 -0400

    added more components.

--- Develop/react-portfolio/src/App.jsx
+++ Develop/react-portfolio/src/App.jsx
@@ -4,9 +4,24 @@ import viteLogo from '/vite.svg'
 import './App.css'
 import Nav from './components/Nav';
 import PortfolioList from './components/PortfolioList';
-import 'bootstrap/dist/css/bootstrap.css';
+import Footer from './components/footer';
+import 'bootstrap/dist/css/bootstrap.min.css';
 
 function App() {
+
+  return (
+    <>
+      <Nav />
+      <PortfolioList />
+      <Footer />
+    </>
+  )
+}
+
+export default App;
+
+
+{/* 
   const [count, setCount] = useState(0)
 
   return (
@@ -16,11 +31,7 @@ function App() {
     </>
   )
 }
-
-export default App;
-
-
-{/* <div>
+<div>
 <a href="https://vitejs.dev" target="_blank">
   <img src={viteLogo} className="logo" alt="Vite logo" />
 </a>
